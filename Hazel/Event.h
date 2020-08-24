2acf913c7a9da94d464be3398202896c873d9b5a
Thomas Frans <thomas_frans@hotmail.be>
2020-08-24 23:32:38 +0200

    Same as previous, forgot to save in VS

--- Hazel/Event.h
+++ Hazel/Event.h
@@ -38,7 +38,14 @@ namespace Hazel {
 		virtual EventType GetEventType() const = 0;
 		virtual const char* GetName() const = 0;
 		virtual int GetCategoryFlags() const = 0;
+		virtual std::string ToString() const { return GetName(); }
 
+		inline bool IsInCategory(EventCategory category)
+		{
+			return GetCategoryFlags() & category;
+		}
+	protected:
+		bool m_Handled = false;
 	};
 
 }
