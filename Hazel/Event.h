d6605cb70d7660034bdfe948872ac51ed0c32be3
Thomas Frans <thomas_frans@hotmail.be>
2020-08-24 23:30:51 +0200

    Temporary commit just for safety and backup, see TODO for where to start

---
+++ Hazel/Event.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "Hazel/Core.h"
+
+#include <string>
+#include <functional>
+
+namespace Hazel {
+
+	// Events in Hazel are currently blocking, meaning when an event occurs it
+	// immediately gets dispatched and must be dealt with right then and there.
+	// For the future, a better strategy might be to buffer events in an event
+	// bus and process them during the "event" part of the update stage.
+
+	enum class EventType
+	{
+		None = 0,
+		WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved,
+		AppTick, AppUpdate, AppRender,
+		KeyPressed, KeyReleased,
+		MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
+	};
+
+	enum EventCategory
+	{
+		None = 0,
+		EventCategoryApplication     = BIT(0),
+		EventCategoryInput           = BIT(1),
+		EventCategoryKeyboard        = BIT(2),
+		EventCategoryMouse           = BIT(3),
+		EventCategoryMouseButton     = BIT(4)
+	};
+
+	class HAZEL_API Event
+	{
+		friend class EventDispatcher;		// Friend just means the EventDispatcher class has direct access to the protected and private parts of this class
+	public:
+		virtual EventType GetEventType() const = 0;
+		virtual const char* GetName() const = 0;
+		virtual int GetCategoryFlags() const = 0;
+
+	};
+
+}
